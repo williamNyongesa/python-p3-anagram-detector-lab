d8b9c86a31a3bbc8eb87d593c638839cb5821b2b
William Nyongesa <marakawilliam@gmail.com>
2023-09-17 10:07:39 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return[w for w in word_list if sorted(w) == sorted(self.word) ]
